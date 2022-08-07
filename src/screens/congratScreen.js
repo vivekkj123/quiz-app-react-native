0591550d2aa6e1e12aa91ad99a953fb141a69800
Vivek K J <vivekkj2004@gmail.com>
2022-08-07 18:18:26 +0530

    Store user's name and score to async storage. [For Leaderboard ]
    Signed-off-by: Vivek K J <vivekkj2004@gmail.com>

--- src/screens/congratScreen.js
+++ src/screens/congratScreen.js
@@ -4,11 +4,34 @@ import {Divider} from 'react-native-elements';
 import PieChart from 'react-native-pie-chart';
 import {Button} from 'react-native-elements';
 import {useStateValue} from '../stateProvider';
-const congratScreen = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
+import Questions from '../questions.json';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+let storeData = async (user, score) => {
+  let currentLeaderboard = await AsyncStorage.getItem('leaderBoard');
+  let newData = {
+    name: user,
+    score: score,
+  };
+  if (currentLeaderboard == null) {
+    currentLeaderboard = [];
+    currentLeaderboard.push(newData);
+  } else {
+    currentLeaderboard = JSON.parse(currentLeaderboard);
+    currentLeaderboard.push(newData);
+  }
+  await AsyncStorage.setItem(
+    'leaderBoard',
+    JSON.stringify(currentLeaderboard),
+  ).catch(() => {
+    console.log('error saving');
+  });
+};
+const CongratScreen = ({navigation}) => {
   const [{user, score}] = useStateValue();
+  storeData(user, score);
   const widthAndHeight = 150;
-  const series = [score, 10 - score];
+  const series = [score, Questions.questions.length - score];
   const sliceColor = ['#00FF00'];
   return (
     <View style={styles.cogratsScreen}>
@@ -20,9 +43,10 @@ const congratScreen = () => {
         series={series}
         sliceColor={sliceColor}
         doughnut={true}
-        coverRadius={0.55}
+        coverRadius={0.7}
         coverFill={'#FFF'}
       />
+      <Text style={styles.scoreStyle}>{score}</Text>
       <Divider width={100} />
       <Text style={styles.congratsText}>Liked it?</Text>
       <Button
@@ -38,7 +62,7 @@ const congratScreen = () => {
   );
 };
 
-export default congratScreen;
+export default CongratScreen;
 
 const styles = StyleSheet.create({
   cogratsScreen: {
@@ -49,5 +73,12 @@ const styles = StyleSheet.create({
   congratsText: {
     fontSize: 26,
     textAlign: 'center',
+    marginVertical: 20,
+  },
+  scoreStyle: {
+    position: 'relative',
+    bottom: 105,
+    fontSize: 45,
+    fontWeight: '800',
   },
 });
